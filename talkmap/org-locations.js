ef6e9b01e1a6d5c867edbcc1de2a8017a9811f8b
Leroy Francois <leroy@fzp.czu.cz>
2022-06-20 14:10:49 +0200

    updating talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
     "Prague",
-    50.0874654,
-    14.4212535
+    50.0596288,
+    14.446459273258009
   ],
   [
     "Braunschweig",
@@ -13,5 +13,10 @@ var addressPoints = [
     "Amsterdam",
     52.3727598,
     4.8936041
+  ],
+  [
+    "Vancouver, BC",
+    49.2608724,
+    -123.113952
   ]
 ];
